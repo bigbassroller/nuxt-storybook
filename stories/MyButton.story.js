2b06e0e5dfeb9a322573271dcc35f0da9a684686
Michael Chavez <micchavez@ebay.com>
2018-08-10 23:24:14 -0700

    intial commit

---
+++ stories/MyButton.story.js
@@ -0,0 +1,19 @@
+
+import { storiesOf } from '@storybook/vue'
+import MyButton from '~/components/Button.vue'
+import Centered from '@storybook/addon-centered'
+
+storiesOf('Button', module)
+  .addDecorator(Centered)
+  .add('rounded button', () => ({
+    template: '<my-button :rounded="true"> 👍  A Button with rounded edges</my-button>'
+  }))
+  .add('normal button', () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="false">A Button with square edges</my-button>'
+  }))
+  // .add('story as a template', () => '<my-button :rounded="true">story as a function template</my-button>')
+  .add('story as a component', () => ({
+    components: { MyButton },
+    template: '<my-button :rounded="true">rounded</my-button>'
+  }));
